efd6d281f66dc93acfed7dd130220d770707557f
Kenny Tran <kennytran@Kennys-MacBook-Air-3.local>
2025-10-11 14:18:45 -0600

    Created SQL query strings for company-page.php

--- company-page.php
+++ company-page.php
@@ -2,6 +2,16 @@
 include('config.inc.php');
 
 $pdo = database();
+
+//Company details
+$sql1 = "SELECT symbol, name, sector, subindustry, address, exchange, website, description,
+       latitude, longitude, financials";
+
+// Historical prices (oldest → newest)
+$sql2 = "SELECT date, open, close, high, low, volume
+FROM history
+WHERE symbol = :symbol
+ORDER BY date ASC";
 ?>
 
 <!DOCTYPE html>
